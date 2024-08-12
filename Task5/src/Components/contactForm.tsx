abeaf69abe3e0b48dd76e5d986b1053eceee3f00
Rabuma Milisha Dugasa <145286734+Rabumamili@users.noreply.github.com>
2024-08-12 14:20:20 +0300

    Update and rename Task5/src/App.js to Task5/src/Components/contactForm.tsx

---
+++ Task5/src/Components/contactForm.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import "../index";
+
+type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const ContactForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    alert("Form submitted successfully!");
+  };
+
+  return (
+    <div className="contact-form-container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Contact Us</h2>
+        <p>
+          Got a question or just want to say hello? 
+          We're all ears, drop us a line below!
+        </p>
+
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Your Name"
+            {...register("name", {
+              required: "Name is required",
+              pattern: {
+                value: /^[A-Za-z\s]+$/,
+                message: "Only alphabets are allowed",
+              },
+            })}
+          />
+          {errors.name && <p className="error">{errors.name.message}</p>}
+        </div>
+
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Your Mail"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          {errors.email && <p className="error">{errors.email.message}</p>}
+        </div>
+
+        <div className="form-group">
+          <textarea
+            placeholder="Your Message"
+            {...register("message", { required: "Message is required" })}
+          ></textarea>
+          {errors.message && <p className="error">{errors.message.message}</p>}
+        </div>
+
+        <button type="submit" className="submit-button">
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
