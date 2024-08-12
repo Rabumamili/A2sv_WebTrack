62f5211d82737bf897520bbb08591983101f92c5
Rabuma Milisha Dugasa <145286734+Rabumamili@users.noreply.github.com>
2024-08-12 14:25:37 +0300

    Create app.tsx

---
+++ Task5/src/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "./components/contactForm";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+};
+
+export default App;
